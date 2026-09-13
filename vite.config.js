37ba3e9b6d83ffa1274ac58fb0187c340e9b2d36
Anhad75 <sonianhad128@gmail.com>
2026-09-13 19:49:15 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/ARC-APP-C/' : '/',
+  base:"/ARC-APP/",
 })
