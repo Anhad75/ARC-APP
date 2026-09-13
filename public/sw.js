282cfa71c71ad6a42f65dbb905dcb55f06570b96
Anhad75 <sonianhad128@gmail.com>
2026-09-13 19:23:54 +0530

    Fix GitHub Pages service worker paths

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
 const CACHE_NAME = 'field-notes-v2'
-const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/favicon.svg']
+const BASE_PATH = new URL('./', self.registration.scope).pathname
+const APP_SHELL = [BASE_PATH, `${BASE_PATH}index.html`, `${BASE_PATH}manifest.webmanifest`, `${BASE_PATH}favicon.svg`]
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)))
@@ -24,6 +25,6 @@ self.addEventListener('fetch', (event) => {
         caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
       }
       return response
-    }).catch(() => event.request.mode === 'navigate' ? caches.match('/index.html') : Response.error())),
+    }).catch(() => event.request.mode === 'navigate' ? caches.match(`${BASE_PATH}index.html`) : Response.error())),
   )
 })
